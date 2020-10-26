4d625badee26981e1cc7105d324ac97973aea825
Miras <sektor.miras@gmail.com>
2020-10-26 19:32:16 +0600

    added: description and product

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'This is an item that you wear on your foot'
+        }
+    }
+})
